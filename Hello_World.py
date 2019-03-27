87b1b0fcf9b290b4915194a849d7034b12697cd4
abhi <abhishekkshk68@gmail.com>
2019-03-27 17:12:34 +0000

    Add the use of browser

--- Hello_World.py
+++ Hello_World.py
@@ -1,14 +1,18 @@
 import aiml
 import os
-
+import wikipedia
 kernel = aiml.Kernel()
 
-if os.path.isfile("bot_5brain.brn"):
-    kernel.bootstrap(brainFile = "bot_5brain.brn")
+if os.path.isfile("bot_newbrain.brn"):
+    kernel.bootstrap(brainFile = "bot_newbrain.brn")
 else:
     kernel.bootstrap(learnFiles = "std-startup.xml", commands = "load aiml b")
-    kernel.saveBrain("bot_5brain.brn")
+    kernel.saveBrain("bot_newbrain.brn")
 
 # kernel now ready for use
 while True:
-    print (kernel.respond(input("Enter your message >> ")))
+    Message =kernel.respond(input("Enter your message >> "))
+    if (Message.find('Search')==True):
+        print(wikipedia.summary('Message'))
+    print(Message)
+
